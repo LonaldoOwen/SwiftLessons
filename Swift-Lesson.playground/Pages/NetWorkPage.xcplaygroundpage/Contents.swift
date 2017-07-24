97bf9bb973a181066b378fa267b651b245af891f
libowen <libowen@cct.cn>
2017-07-24 12:03:09 +0800

    from work mini

--- Swift-Lesson.playground/Pages/NetWorkPage.xcplaygroundpage/Contents.swift
+++ Swift-Lesson.playground/Pages/NetWorkPage.xcplaygroundpage/Contents.swift
@@ -23,7 +23,7 @@ Network.request(method: "POST", url: url, parameters: parameters) { (data, respo
     //
     do {
         if let jsonData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: AnyObject] {
-            print("jsonData: \(String(describing: jsonData))")
+            //print("jsonData: \(String(describing: jsonData))")
         }
     } catch let error {
         print("error: \(error.localizedDescription)")
@@ -31,6 +31,18 @@ Network.request(method: "POST", url: url, parameters: parameters) { (data, respo
     //
     PlaygroundPage.current.finishExecution()
 }
+
+let mp3Url = "https://res.wx.qq.com/voice/getvoice?mediaid=MzIwMjg5Njg5N18yMjQ3NDg0Mjg1"
+//let request = URLRequest()
+let session = URLSession.shared
+let task = session.dataTask(with: URL(string: mp3Url)!) { (data, response, error) in
+    //
+    print("response: \(String(describing: response))")
+    print("data: \(String(describing: data))")
+}
+task.resume()
+    
+
 
 
 
