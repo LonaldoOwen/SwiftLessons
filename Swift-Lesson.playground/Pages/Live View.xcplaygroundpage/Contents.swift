8c1909baff781dc469484ebe8341e1caeb551de7
mirageowen <ctyandmydogs@gmail.com>
2017-08-10 23:13:29 +0800

    使用enum分支练习enum的嵌套用法

---
+++ Swift-Lesson.playground/Pages/Live View.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+import PlaygroundSupport
+
+var str = "Hello, playground"
+
+/// Show live view
+/// 点击Show the Assistant editor就可显示了
+let demoView = RandomColorView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+PlaygroundPage.current.liveView = demoView
+
+
+
+
+
+
+
+
+
+//: [Next](@next)
