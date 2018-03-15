a173b2516c268018a78f359473784a050a8145b8
libowen <libowen@cct.cn>
2018-03-15 19:02:22 +0800

    add some basic saytax: Basic Comparisons,Basic Compound Predicates,String Comparisons,Aggregate Operations

--- Swift-Lesson.playground/Pages/NSPredicate.xcplaygroundpage/Contents.swift
+++ Swift-Lesson.playground/Pages/NSPredicate.xcplaygroundpage/Contents.swift
@@ -59,6 +59,25 @@ let persons = [zhangsan, lisi, wangwu]
 let lastNameSearchString = "abcd"
 let birthdaySearchDate = Date()
 //let predicate = NSPredicate(format: "lastName like[cd] %@ AND birthday > %@", lastNameSearchString, birthdaySearchDate as CVarArg)
+
+
+
+
+
+
+
+/*:
+ ## Parser Basics
+ 
+ 
+ */
+/// 使用%K报错？？？
+/// %K: key path，用来替换变量名;
+/// %@: object value，用来替换变量值
+/// $VARIABLE_NAME: 用来表示Variables
+
+/// LIKE用法
+// lastName LIKE lastNameSearchString
 let predicate = NSPredicate(format: "lastName like %@", lastNameSearchString)
 // 报错：“error: Execution was interrupted, reason: signal SIGABRT.” ？？？
 // 原因：NSPredicate应用需要满足object“class must be key-value coding compliant for the keys you want to use in a predicate.”
@@ -67,11 +86,204 @@ let result = predicate.evaluate(with: zhangsan)
 
 let zhangsanName = "zhangsan"
 let zhangsanLastName = "zhang"
-let cPredicate = NSPredicate(format: "%k like %@", zhangsan.lastName, zhangsanLastName)
+// zhangsan.lastName LIKE zhangsanLastName
+let cPredicate = NSPredicate(format: "%@ like %@", zhangsan.lastName, zhangsanLastName)
 let cResult = cPredicate.evaluate(with: zhangsan)
 
+// 定义propertyName用来存储属性名， value存储值
+let propertyName = "lastName"
+let value = "zhang"
+let dPredicate = NSPredicate(format: "%K like %@", propertyName, value)
+let dResult = dPredicate.evaluate(with: zhangsan)
+
+// age > $AGE, 应用场景？？？
+let dollarPredicateTemp = NSPredicate(format: "age > $AGE")
+let dollarPredicate = dollarPredicateTemp.withSubstitutionVariables(["AGE": 5])
+let dollarResult = dollarPredicate.evaluate(with: zhangsan)
+
+
+/*:
+ ## Basic Comparisons（比较运算符）
+ 
+ =, ==
+ - The left-hand expression is equal to the right-hand expression.
+ 
+ ">=, =>"
+ - The left-hand expression is greater than or equal to the right-hand expression.
+ 
+ <=, =<
+ - The left-hand expression is less than or equal to the right-hand expression.
+ 
+ ">"
+ - The left-hand expression is greater than the right-hand expression.
+ 
+ <
+ - The left-hand expression is less than the right-hand expression.
+
+ !=, <>
+ - The left-hand expression is not equal to the right-hand expression.
+ 
+ 
+ */
+/// =, == 用法
+let equalPredicate = NSPredicate(format: "age = 10")
+equalPredicate.evaluate(with: zhangsan)
+let doubleEqualPredicate = NSPredicate(format: "age == 20")
+doubleEqualPredicate.evaluate(with: zhangsan)
+
+/// > 用法
+// age > 10
+let greaterPredicate = NSPredicate(format: "age > 10")
+greaterPredicate.evaluate(with: zhangsan)
+
+
+/*:
+ ## Boolean Value Predicates
+ TRUEPREDICATE
+ A predicate that always evaluates to TRUE.
+ FALSEPREDICATE
+ A predicate that always evaluates to FALSE.
+
+ 
+ */
+
+
+/*:
+ ## Basic Compound Predicates（复合断言）
+ AND, &&
+ - Logical AND.
+ 
+ OR, ||
+ - Logical OR.
+ 
+ NOT, !
+ - Logical NOT.
+
+ */
+
+// age > 5 AND age < 30
+let andPredicate = NSPredicate(format: "age > 5 && age < 30")
+let andResult = andPredicate.evaluate(with: zhangsan)
+
+// lastName == "zhang" OR age > 5
+let orPredicate = NSPredicate(format: "lastName = 'zhang' OR age > 5")
+let orResult = orPredicate.evaluate(with: zhangsan)
+
+//
+let notPredicate = NSPredicate(format: "NOT firstName = 'sansan'")
+let notResult = notPredicate.evaluate(with: zhangsan)
+
+
+/*:
+ ## String Comparisons (字符串比较)
+ String comparisons are, by default, case and diacritic sensitive. You can modify an operator using the key characters c and d within square braces to specify case and diacritic insensitivity respectively, for example firstName BEGINSWITH[cd] $FIRST_NAME.
+ 
+ BEGINSWITH
+ - The left-hand expression begins with the right-hand expression.
+ 
+ CONTAINS
+ - The left-hand expression contains the right-hand expression.
+ 
+ ENDSWITH
+ - The left-hand expression ends with the right-hand expression.
+ 
+ LIKE
+ - The left hand expression equals the right-hand expression: ? and * are allowed as wildcard characters, where ? matches 1 character and * matches 0 or more characters.
+ 
+ UTI-CONFORMS-TO
+ - ???
+ 
+ */
+
+// BEGINSWITH：
+// lastName BEGINSWITH "zh"（表示：lastName是以 “zh” 开头的）
+let beginwithPredicate = NSPredicate(format: "lastName BEGINSWITH 'zh'")
+let beginwithResult = beginwithPredicate.evaluate(with: zhangsan)
+
+// CONTAINS
+// lastName CONTAINS "a"（表示：lastName 包含 "a"）
+let containsPredicate = NSPredicate(format: "lastName CONTAINS 'a'")
+let containsResult = containsPredicate.evaluate(with: zhangsan)
+
+// ENDSWITH
+// lastName ENDSWITH "ang"（表示：lastName 以 "ang" 结尾）
+let endwithPredicate = NSPredicate(format: "lastName ENDSWITH 'ang'")
+let endwithResult = endwithPredicate.evaluate(with: zhangsan)
+
+// LIKE
+// lastName LIKE "zhang"（lastName 和 "zhang" 相等）
+let likePredicate = NSPredicate(format: "lastName LIKE 'zhang'")
+let likeResult = likePredicate.evaluate(with: zhangsan)
+
+// ?: matches 1 character（？可以代表一个任意字符）
+let likePredicate2 = NSPredicate(format: "lastName LIKE '?hang'")
+let likeResult2 = likePredicate2.evaluate(with: zhangsan)
+
+// *: （* 可以代表0个或更多个字符）
+let likePredicate3 = NSPredicate(format: "lastName LIKE 'z*g'")
+let likeResult3 = likePredicate2.evaluate(with: zhangsan)
+
+
+/*:
+ ## Aggregate Operations（集合操作）
+ 
+ ANY, SOME
+ - Specifies any of the elements in the following expression. For example ANY children.age < 18.
+ 
+ ALL
+ - Specifies all of the elements in the following expression. For example ALL children.age < 18.
+ 
+ NONE
+ - Specifies none of the elements in the following expression. For example, NONE children.age < 18. This is logically equivalent to NOT (ANY ...).
+ 
+ IN
+ - Equivalent to an SQL IN operation, the left-hand side must appear in the collection specified by the right-hand side.
+ - For example, name IN { 'Ben', 'Melissa', 'Nick' }. The collection may be an array, a set, or a dictionary—in the case of a dictionary, its values are used.
+ - In Objective-C, you could create a IN predicate as shown in the following example:
+ 
+ */
+
+// ANY, SOME
+// ANY age < 40（表示：集合中，只要有 age < 40 的，结果就为true）
+let anyPredicate = NSPredicate(format: "ANY age < 40")
+let anyResult = anyPredicate.evaluate(with: persons)
+
+// ALL
+// ALL age < 40（表示：集合中，所有元素都有满足 age < 40 时，结果才为true）
+let allPredicate = NSPredicate(format: "ALL age < 40")
+let allResult = allPredicate.evaluate(with: persons)
+
+// NONE
+// NONE age < 10 == NOT ANY age < 10（表示，集合中，所有元素都不满足 age < 10 时，结果才为true）
+let nonePredicate = NSPredicate(format: "NONE age < 10")
+let noneResult = nonePredicate.evaluate(with: persons)
+
+// IN
+// lastName IN {"zhang", "li", "wang"}（表示：lastName在集合{"zhang", "li", "wang"}中）
+let inPredicate = NSPredicate(format: "lastName IN %@", ["zhang", "li", "wang"])
+let inResult = inPredicate.evaluate(with: zhangsan)
+
+/// IN用法
+// SELF IN {"Stig", "Shaffiq", "Chris"}
 let bPredicate = NSPredicate(format: "SELF IN %@", ["Stig", "Shaffiq", "Chris"])
 let bresult = bPredicate.evaluate(with: "Shaffiq")
+
+
+/*:
+ ## Literals
+ Single and double quotes produce the same result, but they do not terminate each other. For example, "abc" and 'abc' are identical, whereas "a'b'c" is equivalent to a space-separated concatenation of a, 'b', c.
+ 
+ 
+ */
+
+
+
+
+/*:
+ ##
+ 
+ 
+ */
 
 
 
