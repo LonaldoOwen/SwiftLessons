bae00cd654589d27721bc3ba2729f4ab3f5bdf08
owen <ctyandmydogs@sina.com>
2018-06-07 23:47:14 +0800

    复习Generic functions

--- Generic.playground/Contents.swift
+++ Generic.playground/Contents.swift
@@ -9,6 +9,32 @@ import Foundation
 
 /// Generic
 
+
+
+/// Generic Functions
+
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoValues(&someInt, &anotherInt)
+// someInt is now 107, and anotherInt is now 3
+
+var someString = "hello"
+var anotherString = "world"
+swapTwoValues(&someString, &anotherString)
+// someString is now "world", and anotherString is now "hello"
+
+
+
+/// 
+
+
+
 // Type Constraints
 func findIndex(ofString valueToFind: String, in array: [String]) -> Int? {
     for (index, value) in array.enumerated() {
